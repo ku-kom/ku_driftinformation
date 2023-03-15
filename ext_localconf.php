4957937d4ee70eddf7da8a35b278c0ec8156a330
nanna-dk <nanna@e-nanna.dk>
2023-03-15 15:12:45 +0100

    Setup

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,6 +11,7 @@ defined('TYPO3') or die('Access denied.');
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
@@ -21,12 +22,12 @@ if ($versionInformation->getMajorVersion() < 12) {
 }
 
 // Register plugin
-
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-
 ExtensionUtility::configurePlugin(
   'ku_driftinformation',
   'Pi1',
   [\UniversityOfCopenhagen\KuDriftinformation\Controller\DriftinfoController::class => 'list'],
   [\UniversityOfCopenhagen\KuDriftinformation\Controller\DriftinfoController::class => 'list']
 );
+
+// KU driftinformation Viewhelper namespace
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['kuDriftinfo'] = ['UniversityOfCopenhagen\KuDriftinformation\ViewHelpers'];
